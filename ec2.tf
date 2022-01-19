221293079a32551c5a4a685ec3e76ed639b9ec41
adampeklay <adam@peklay.com>
2022-01-18 19:31:52 -0600

    removed old key, shouldn't have been there anyways

--- ec2.tf
+++ ec2.tf
@@ -33,5 +33,5 @@ module "kube-worker" {
 // ec2 instance key pair - refrerence any local keypair you wish to use
 resource "aws_key_pair" "cka_lab" {
   key_name   = "cka_lab"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKqRlh/p8tRQioJLxk8W/+dfc0ynQDQMrGLaGmMb4qfHj+OSPiovoI2A1pQiXB1CUKNgCnp+9nsnCj0Dl/P+JUdtzcD07jT2B6fMRyXb19hexpBXxjWPvV/NpCAV94rszafXxTktQ4+1ar0HN+AZQgwBel34RTA2EUJvtHu2nmWQIUcLQeoBSFfCKo8k1hgaOZudHII1Z9DKQUweioBIvBPDPN3u8q3konApRXGB6rn/lHrBwn+J4rAw6EvSFqlKUFvJk7LJXS2j2In9EQWnbVG2XdzYEv7HBsZEAx5bxxOMxb1r3P1XYufbR9YwAYQKTRzckjwaF8upamZdO6N23OLMscfXFJcCoLs+6MR/kaIe2HZzI6WFuBnNIq/QMsjHZjNQpHujEG62l/b0PGfZ/iVq+969Ztk+eJ5NDZhYIIPE8tQZgZcCjPfvw7/tbLgTzLc/xS/tallAZOTOGI5cUVJ+e8515Hq+0fbw/+V1D5/7eItpIZChUE3KQcU1qEMl8="
+  public_key = "<redacted>"
 }
