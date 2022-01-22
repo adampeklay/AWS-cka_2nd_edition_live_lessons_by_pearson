636561307172a8f85ebb6ccda86cabc36fd4f07f
adampeklay <adam@peklay.com>
2022-01-22 11:52:05 -0600

    forgot to save and terraform fmt

--- variables.tf
+++ variables.tf
@@ -20,34 +20,34 @@ variable "worker_instance_type" {
 variable "my_current_ip" {
   type        = string
   description = "LOCAL ENV VAR: Needed for ingress security group"
-  //export TF_var_
+  //export TF_VAR_my_current_ip=$(curl -4 icanhazip.com)/\32
 }
 
 variable "cidr" {
-  type = string
+  type        = string
   description = "the CIDR block for the lab"
-  default = "192.168.0.0/16"
+  default     = "192.168.0.0/16"
 }
 
 variable "private_subnets" {
-  type = list
+  type        = list(any)
   description = "The private subnet the lab will use"
-  default = ["192.168.4.0/24"]
+  default     = ["192.168.4.0/24"]
 }
 
 variable "public_subnets" {
-  type = list
+  type        = list(any)
   description = "The public subnet the lab will use"
-  default = ["192.168.104.0/24"]
+  default     = ["192.168.104.0/24"]
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "The AWS region to deploy the cluster in"
 }
 
 variable "public_key" {
-  type = string
+  type        = string
   description = "LOCAL ENV VAR: used for ssh key based auth to the k8s controller node"
   //export TF_var_public_key="your public key in quotes"
 }
