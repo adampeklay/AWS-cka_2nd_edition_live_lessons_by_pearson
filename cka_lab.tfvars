8a6b139076a44c45f309f56949530fcd5bd16bf5
Adam Peklay <13510667+adampeklay@users.noreply.github.com>
2022-01-22 11:43:49 -0600

    fixed vars, use local vars, readme updates, WIP (#12)

--- cka_lab.tfvars
+++ cka_lab.tfvars
@@ -1,2 +1,1 @@
-my_current_ip = "<redacted>"
 region = "us-east-1a"
--- variables.tf
+++ variables.tf
@@ -19,7 +19,8 @@ variable "worker_instance_type" {
 
 variable "my_current_ip" {
   type        = string
-  description = "Needed for ingress security group"
+  description = "LOCAL ENV VAR: Needed for ingress security group"
+  //export TF_var_
 }
 
 variable "cidr" {
@@ -43,6 +44,12 @@ variable "public_subnets" {
 variable "region" {
   type = string
   description = "The AWS region to deploy the cluster in"
+}
+
+variable "public_key" {
+  type = string
+  description = "LOCAL ENV VAR: used for ssh key based auth to the k8s controller node"
+  //export TF_var_public_key="your public key in quotes"
 }
 
 // variablize tags too
