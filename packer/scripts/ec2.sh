4e37e4b71a59566c6bc6442c95f7ed3067997692
adampeklay <adam@peklay.com>
2022-01-30 20:36:21 -0600

    finalized packer scripts

--- packer/scripts/ec2.sh
+++ packer/scripts/ec2.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-##########################################################################################
-#  This script finishes configuring the ec2 instances per the instructors requirements   #
-#                                       -----------                                      #
-# - Packer sets an at job to run this script when an ec2 instnce is created from the ami #
-# - You can run the script again if need be                                              #
-# - If the script had errors, a file will be written in your home directory              #
-#   - - See `else` --> `echo` below for details                                          #
-#                                       -----------                                      #
-##########################################################################################
+#############################################################################################
+#    This script finishes configuring the ec2 instances per the instructors requirements    #
+#                                       -----------                                         #
+#  Packer sets an `at`` job to run this script when an ec2 instnce is created from the ami  #
+#                                       -----------                                         #
+#############################################################################################
 
 set -x
 
@@ -23,44 +20,46 @@ WORKER_3_IP="192.168.4.113"
 # Functions
 
 append_hosts_file () {
-  # Declare associate array
-  declare -A ENTRIES=( [controller]=$CONTROLLER_IP [worker-1]=$WORKER_1_IP [worker-2]=$WORKER_2_IP [worker-3]=$WORKER_3_IP )
-  # backup original /etc/hosts file to $original_hosts, in case script needs to be ran ad hoc
-  orginal_hosts="~/hosts_orig"
-  staged_hosts="~/hosts
-  # Set /etc/hosts
-  if [ -n $original_hosts ]; then
-    $(touch $staged_hosts && > $staged_hosts)
-  else
-    $(sudo cp /etc/hosts $original_hosts)
-    $(sudo cp /etc/hosts $original_hosts)
-  fi
-  # call arrary to update file
+  declare -A ENTRIES=( [controller]=${CONTROLLER_IP} [worker-1]=${WORKER_1_IP} [worker-2]=${WORKER_2_IP} [worker-3]=${WORKER_3_IP} )
   for entries in ${!ENTRIES[@]}; do
-    $(sudo echo "$entries   ${ENTRIES[$entries]}" >> $staged_hosts); 
+    echo "$entries   ${ENTRIES[$entries]}" | sudo tee -a /etc/hosts; 
   done
-  # hard update /etc/hosts
-  $(sudo mv $staged_hosts /etc/hosts)
 }
 
-#############################################################
-# Update /etc/hosts, and set hostnames on cluster instances #
-#############################################################
+#####################
+# Update /etc/hosts #
+#####################
 
-if [[ $(append_hosts_file) ]]; then
-  echo "/etc/hosts updated"
+append_hosts_file
+
+if [ $? -eq 0 ]; then
+  echo "/etc/hosts updated successfully"
 else
-  echo "investigate, error updating /etc/hosts" > "$HOME/script_error.txt"
+  echo "investigate, error running append_hosts function"
+  exit 1
 fi
 
-if [[ $LOCAL_IP == $CONTROLLER_IP ]];then
-  sudo hostnamectl set-hostname "controller"
-elif [[ $LOCAL_IP == $WORKER_1_IP ]];then
-  sudo hostnamectl set-hostname "worker-1"
-elif [[ $LOCAL_IP == $WORKER_2_IP ]];then
-  sudo hostnamectl set-hostname "worker-2"
-elif [[ $LOCAL_IP == $WORKER_3_IP ]];then  
-  sudo hostnamectl set-hostname "worker-3" 
+######################################
+# Set hostnames on cluster instances #
+######################################
+
+if [[ $LOCAL_IP == $CONTROLLER_IP ]]; then
+  sudo hostnamectl set-hostname controller
+  echo "hostname set successfully"
+elif [[ $LOCAL_IP == $WORKER_1_IP ]]; then
+  sudo hostnamectl set-hostname worker-1
+  echo "hostname set successfully"
+elif [[ $LOCAL_IP == $WORKER_2_IP ]]; then
+  sudo hostnamectl set-hostname worker-2
+  echo "hostname set successfully"
+elif [[ $LOCAL_IP == $WORKER_3_IP ]]; then
+  sudo hostnamectl set-hostname worker-3
+  echo "hostname set successfully"
 else
-  echo "investigate, error updating hostnames" > "$HOME/script_error.txt"
+  echo "investigate, error updating hostname(s)"
+  exit 1
 fi
+
+echo "script completed succesfully"
+
+exit 0
