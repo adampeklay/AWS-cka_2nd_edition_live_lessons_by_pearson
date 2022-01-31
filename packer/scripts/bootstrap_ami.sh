c8b797a239c9f5c8bf9ab81531200acaac0428c5
Adam Peklay <13510667+adampeklay@users.noreply.github.com>
2022-01-30 23:38:44 -0600

    Update bootstrap_ami.sh (#26)

--- packer/scripts/bootstrap_ami.sh
+++ packer/scripts/bootstrap_ami.sh
@@ -25,14 +25,14 @@ packages=("vim" "git" "bash-completion" "at")
 # Install required packges #
 ############################
 
-logger ": ${SCRIPT} : installing: ${packages[@]}"
+logger ": $SCRIPT : installing: ${packages[@]}"
 
 sudo yum install "${packages[@]}" -y
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : packages installed successfully"
+  logger ": $SCRIPT : packages installed successfully"
 else
-  logger ": ${SCRIPT} : investigate, error installing required packages"
+  logger ": $SCRIPT : investigate, error installing required packages"
   exit 1
 fi
 
@@ -40,14 +40,14 @@ fi
 # Enable and start the at deamon #
 ##################################
 
-logger ": ${SCRIPT} : enabling and starting atd"
+logger ": $SCRIPT : enabling and starting atd"
 
 sudo systemctl enable --now atd
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : atd enabled and started succesfully"
+  logger ": $SCRIPT : atd enabled and started succesfully"
 else
-  logger ": ${SCRIPT} : investigate, error enabling and starting atd"
+  logger ": $SCRIPT : investigate, error enabling and starting atd"
   exit 1
 fi
 
@@ -55,14 +55,14 @@ fi
 # Clone the instructors repo #
 ##############################
 
-logger ": ${SCRIPT} : cloning the required git rep: $REPO"
+logger ": $SCRIPT : cloning the required git rep: $REPO"
 
 git clone $REPO $REPO_DIR
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : git repo cloned successfully"
+  logger ": $SCRIPT : git repo cloned successfully"
 else
-  logger ": ${SCRIPT} : investigate, error cloning github repo"
+  logger ": $SCRIPT : investigate, error cloning github repo"
   exit 1
 fi
 
@@ -70,15 +70,15 @@ fi
 # Run the instructors scripts #
 ###############################
 
-logger ": ${SCRIPT} : running instructor provided lab setup scripts"
+logger ": $SCRIPT : running instructor provided lab setup scripts"
 
 sudo bash ${REPO_DIR}/${CONTAINER_LAB_SCRIPT} && \
 sudo bash ${REPO_DIR}/${K8S_LAB_SCRIPT}
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : lab scripts completed succesfully"
+  logger ": $SCRIPT : lab scripts completed succesfully"
 else
-  logger ": ${SCRIPT} : investigate, error running instructor provided lab scripts"
+  logger ": $SCRIPT : investigate, error running instructor provided lab scripts"
   exit 1
 fi
 
@@ -86,14 +86,14 @@ fi
 # Cleanup - remove github repo directory #
 ##########################################
 
-logger ": ${SCRIPT} : deleting $HOME/${REPO_DIR}"
+logger ": $SCRIPT : deleting ${HOME}/${REPO_DIR}"
 
-sudo rm -rf $HOME/${REPO_DIR}
+sudo rm -rf ${HOME}/${REPO_DIR}
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : $HOME/${REPO_DIR} deletion success"
+  logger ": $SCRIPT : ${HOME}/${REPO_DIR} deletion success"
 else
-  logger ": ${SCRIPT} : $HOME/${REPO_DIR} deletion failure"
+  logger ": $SCRIPT : ${HOME}/${REPO_DIR} deletion failure"
   exit 1
 fi
 
@@ -101,12 +101,12 @@ fi
 # Create cluster ssh keypair #
 ##############################
 
-ssh-keygen -t rsa -b 2048 -N '' -f $HOME/.ssh/${CLUSTER_KEY}
+ssh-keygen -t rsa -b 2048 -N '' -f ${HOME}/.ssh/${CLUSTER_KEY}
 
 if [ $? -eq 0 ]; then
-  logger ": ${SCRIPT} : $CLUSTER_KEY keypair created in $HOME/.ssh/ successfully"
+  logger ": $SCRIPT : $CLUSTER_KEY keypair created in ${HOME}/.ssh/ successfully"
 else
-  logger ": ${SCRIPT} : investigate, error creating ssh keypair"
+  logger ": $SCRIPT : investigate, error creating ssh keypair"
   exit 1
 fi
 
@@ -117,12 +117,12 @@ fi
 sudo at now +2 minute -f $HOME/ec2.sh
 
 if [ $? -eq 0 ]; then
-   logger ": ${SCRIPT} : at job scheduled successfully, hostnames will be set upon ec2 creation"
+   logger ": $SCRIPT : at job scheduled successfully, hostnames will be set upon ec2 creation"
 else
-   logger ": ${SCRIPT} : investigate, error setting at job"
+   logger ": $SCRIPT : investigate, error setting at job"
    exit 1
 fi
 
-logger ": ${SCRIPT} : finished script ${HOME}/${SCRIPT}"
+logger ": $SCRIPT : finished script ${HOME}/${SCRIPT}"
 
 exit 0
